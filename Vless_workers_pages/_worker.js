5eebdda0dc69cd15df682b6ccdf243f8b18b7610
Jobs-Devin <30886537+zhangzeyu007@users.noreply.github.com>
2025-09-04 10:58:15 +0800

    Update _worker.js
    跟新

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,9 +4,9 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "9f775482-e840-4529-ab91-67d2d5e4c27e";
 
-const proxyIPs = [""];
+const proxyIPs = ["104.18.0.0"];
 const cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
